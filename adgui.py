c3dc14a7afaa4496684f3ac681bb3c1af561e22a
Kerry Wang <zwang34@email.wm.edu>
2021-11-12 15:51:01 -0500

    added analog channel configuration

--- adgui.py
+++ adgui.py
@@ -1,15 +1,39 @@
 import dearpygui.dearpygui as dpg
-
+# Constants
 NUMBER_OF_PAGES = 13
 NUM_ANALOG_CHANNELS = 40
 NUM_DIGITAL_CHANNELS = 64
 NUM_COLUMNS = 20
+TABLE_POLICY_PROP = {
+                    'header_row' : False,
+                    'borders_innerH' : True,
+                    'borders_outerH' : True,
+                    'borders_innerV' : True,
+                    'borders_outerV' : True,
+                    'policy' : dpg.mvTable_SizingStretchProp
+                    }
+TABLE_POLICY_SAME = {
+                    'header_row' : False,
+                    'borders_innerH' : True,
+                    'borders_outerH' : True,
+                    'borders_innerV' : True,
+                    'borders_outerV' : True,
+                    'policy' : dpg.mvTable_SizingStretchSame
+                    }
 
+# Internal data stored as lists                    
 layout = dict()
 pages = [f'Page {i}' for i in range(NUMBER_OF_PAGES)]
 units = ['Unit'] * NUM_ANALOG_CHANNELS
+analog_channel_names = [('Name', i) for i in range(NUM_ANALOG_CHANNELS)]
 
 def layout_windows():
+    '''
+    Moves the windows into default positions. Should be called after first full frame
+    is rendered, as item dimensions may not be computed before rendering. For some
+    reason the menu bar takes up the first frame, so technically should be called
+    after the second frame.
+    '''
     if 'page_selector_y' not in layout:
         layout['page_selector_y'] = dpg.get_item_height('menubar')
     if 'page_selector_width' not in layout:
@@ -30,6 +54,10 @@ def layout_windows():
 
 
 def rename_callback(selector, app_data, user_data):
+    '''
+    Callback for renaming units and pages. Parses object name and updates
+    the internal lists accordingly.
+    '''
     input, parent, popup = user_data
     # Update internal data arrays
     args = str(parent).split(';')
@@ -38,8 +66,19 @@ def rename_callback(selector, app_data, user_data):
             units[int(args[1])] = str(dpg.get_value(input))
         elif args[0] == 'page':
             pages[int(args[1])] = str(dpg.get_value(input))
-            
     dpg.configure_item(parent, label=dpg.get_value(input))
+    dpg.configure_item(popup, show=False)
+
+def configure_channel(selector, app_data, user_data):
+    name, channel, new_name, new_channel, popup = user_data
+    new_name = str(dpg.get_value(new_name))
+    new_channel = int(dpg.get_value(new_channel))
+    args = str(name).split(';')
+    if len(args) > 1:
+        if args[0] == 'analog_channel':
+            analog_channel_names[int(args[1])] = (new_name, new_channel)
+    dpg.configure_item(name, label=new_name)
+    dpg.configure_item(channel, label=new_channel)
     dpg.configure_item(popup, show=False)
 
 dpg.create_context()
@@ -54,7 +93,7 @@ with dpg.viewport_menu_bar(tag='menubar'):
 with dpg.window(tag='page_selector', label='Page Selector', no_close=True, width=dpg.get_viewport_width()):
     with dpg.group(horizontal=True):
         with dpg.group():
-            dpg.add_button(label='Run Cycle', callback=lambda: print(pages))
+            dpg.add_button(label='Run Cycle', callback=lambda: print(analog_channel_names))
             dpg.add_button(label='Stop')
             dpg.add_button(label='Scan')
             dpg.add_checkbox(label='Repeat')
@@ -72,17 +111,19 @@ with dpg.window(tag='tables', label='Tables', no_close=True, width=dpg.get_viewp
             with dpg.group(width=150):
                 dpg.add_spacer(height=57)
                 dpg.add_text('Analog Channels | Line')
-                with dpg.table(header_row=False, borders_innerH=True, borders_outerH=True, borders_innerV=True,
-                               borders_outerV=True, policy=dpg.mvTable_SizingStretchProp):
+                with dpg.table(**TABLE_POLICY_PROP):
                     dpg.add_table_column()
                     dpg.add_table_column()
                     for row in range(NUM_ANALOG_CHANNELS):
                         with dpg.table_row():
-                            dpg.add_selectable(label='Name', span_columns=True)
-                            dpg.add_selectable(label=f'{row + 1}')
+                            name = dpg.add_selectable(label='Name', tag=f'analog_channel;{row}', span_columns=True)
+                            channel = dpg.add_selectable(label=f'{row + 1}')
+                            with dpg.popup(name) as popup:
+                                new_name = dpg.add_input_text(width=75, hint='Name')
+                                new_channel = dpg.add_input_int(width=75, default_value=row+1, step=0, min_value=1, max_value=132, min_clamped=True, max_clamped=True)
+                                dpg.add_button(label='Rename', user_data=(name, channel, new_name, new_channel, popup), callback=configure_channel)
                 dpg.add_text('Digital Channels | Line')
-                with dpg.table(header_row=False, borders_innerH=True, borders_outerH=True, borders_innerV=True,
-                               borders_outerV=True, policy=dpg.mvTable_SizingStretchProp):
+                with dpg.table(**TABLE_POLICY_PROP):
                     dpg.add_table_column()
                     dpg.add_table_column()
                     for row in range(NUM_DIGITAL_CHANNELS):
@@ -96,8 +137,7 @@ with dpg.window(tag='tables', label='Tables', no_close=True, width=dpg.get_viewp
                         with dpg.popup(tab) as popup:
                             dpg.add_input_text(width=75, hint=f'Page {i}')
                             dpg.add_button(label='Rename', user_data=(dpg.last_item(), tab, popup), callback=rename_callback)
-                        with dpg.table(header_row=False, borders_innerH=True, borders_outerH=True, borders_innerV=True,
-                                        borders_outerV=True, policy=dpg.mvTable_SizingStretchProp):
+                        with dpg.table(**TABLE_POLICY_SAME):
                             for col in range(NUM_COLUMNS):
                                 dpg.add_table_column()
                             with dpg.table_row():
@@ -107,8 +147,7 @@ with dpg.window(tag='tables', label='Tables', no_close=True, width=dpg.get_viewp
                                 for col in range(NUM_COLUMNS):
                                     dpg.add_selectable(label="Time")
                         dpg.add_text('Analog Table')
-                        with dpg.table(header_row=False, borders_innerH=True, borders_outerH=True, borders_innerV=True,
-                                        borders_outerV=True, policy=dpg.mvTable_SizingStretchProp):
+                        with dpg.table(**TABLE_POLICY_SAME):
                             for col in range(NUM_COLUMNS):
                                 dpg.add_table_column()
                             for row in range(NUM_ANALOG_CHANNELS):
@@ -116,8 +155,7 @@ with dpg.window(tag='tables', label='Tables', no_close=True, width=dpg.get_viewp
                                     for col in range(NUM_COLUMNS):
                                         dpg.add_selectable(label="Cell")
                         dpg.add_text('Digital Table')
-                        with dpg.table(header_row=False, borders_innerH=True, borders_outerH=True, borders_innerV=True,
-                                        borders_outerV=True, policy=dpg.mvTable_SizingStretchProp):
+                        with dpg.table(**TABLE_POLICY_SAME):
                             for col in range(NUM_COLUMNS):
                                 dpg.add_table_column()
                             for row in range(NUM_DIGITAL_CHANNELS):
@@ -128,18 +166,16 @@ with dpg.window(tag='tables', label='Tables', no_close=True, width=dpg.get_viewp
             with dpg.group(width=100):
                 dpg.add_spacer(height=57)
                 dpg.add_text('Analog Units')
-                with dpg.table(header_row=False, borders_innerH=True, borders_outerH=True, borders_innerV=True,
-                               borders_outerV=True):
+                with dpg.table(**TABLE_POLICY_PROP):
                     dpg.add_table_column()
                     for row in range(NUM_ANALOG_CHANNELS):
                         with dpg.table_row():
-                            parent = dpg.add_selectable(label='Unit', tag=f'unit;{row}')
+                            parent = dpg.add_selectable(label='Unit', tag=f'unit;{row}', callback=lambda s: dpg.set_value(s, False))
                             with dpg.popup(parent) as popup:
                                 dpg.add_input_text(width=75, hint='Unit')
                                 dpg.add_button(label='Rename', user_data=(dpg.last_item(), parent, popup), callback=rename_callback)
                 dpg.add_text('Scan Values')
-                with dpg.table(header_row=False, borders_innerH=True, borders_outerH=True, borders_innerV=True,
-                               borders_outerV=True):
+                with dpg.table(**TABLE_POLICY_PROP):
                     dpg.add_table_column()
                     for row in range(32):
                         with dpg.table_row():
